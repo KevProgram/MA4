4291a3eee59dfc3462aae2e7109eb6bd4fbad751
KevProgram <salazar-kevin@hotmail.com>
2022-10-22 17:58:23 +0200

    c++

--- UPPMA4/person.py
+++ UPPMA4/person.py
@@ -1,38 +1,63 @@
 """ Python interface to the C++ Person class """
 from numba import njit
 import ctypes
-lib = ctypes.cdll.LoadLibrary('./libperson.so')
+from time import perf_counter as pc
 
-class Person(object):
-	def __init__(self, age):
-		lib.Person_new.argtypes = [ctypes.c_int]
-		lib.Person_new.restype = ctypes.c_void_p
-		lib.Person_get.argtypes = [ctypes.c_void_p]
-		lib.Person_get.restype = ctypes.c_int
-		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
-		lib.Person_delete.argtypes = [ctypes.c_void_p]
-		self.obj = lib.Person_new(age)
+# lib = ctypes.cdll.LoadLibrary('./libperson.so')
 
-	def get(self):
-		return lib.Person_get(self.obj)
+# class Person(object):
+# 	def __init__(self, age):
+# 		lib.Person_new.argtypes = [ctypes.c_int]
+# 		lib.Person_new.restype = ctypes.c_void_p
+# 		lib.Person_get.argtypes = [ctypes.c_void_p]
+# 		lib.Person_get.restype = ctypes.c_int
+# 		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
+# 		lib.Person_delete.argtypes = [ctypes.c_void_p]
+# 		self.obj = lib.Person_new(age)
 
-	def set(self, age):
-		lib.Person_set(self.obj, age)
+# 	def get(self):
+# 		return lib.Person_get(self.obj)
+
+# 	def set(self, age):
+# 		lib.Person_set(self.obj, age)
         
-	def __del__(self):
-		return lib.Person_delete(self.obj)
+# 	def __del__(self):
+# 		return lib.Person_delete(self.obj)
+_age= 10
 
-def main():
-	def fib_py(n):
-		if n <= 1:
-			return n
-		else:
-			return (fib_py(n-1) + fib_py(n-2))
-	@njit
-	def fib_numba(n):
-		if n <= 1:
-                        return n
-                else:
-                        return (fib_py(n-1) + fib_py(n-2))
-_age=10
 
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return (fib_py(n-1) + fib_py(n-2))
+
+
+@njit
+def fib_numba(n):
+	if n <=1:
+		return n
+	else:
+		return (fib_numba(n-1) + fib_numba(n-2))
+
+f=Person(n)
+f.fib()
+
+start=pc()
+print(fib_py(_age))
+end=pc()
+print("fib_py:", end-start)
+start=pc()
+print(fib_numba(_age))
+end=pc()
+print("numba: ", end-start)
+print(fib(10))
+
+
+# n= Person(_age)
+
+
+
+
+
+	
