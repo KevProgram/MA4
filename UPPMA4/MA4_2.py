3bc43eb78a0832f73a314faa7e80d0b9889dea89
KevProgram <salazar-kevin@hotmail.com>
2022-10-23 13:49:52 +0200

    Python fixed Ma4_2

--- UPPMA4/MA4_2.py
+++ UPPMA4/MA4_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.9
-
+from numba import njit
+from time import perf_counter as pc
 from person import Person
 
 def main():
@@ -7,6 +8,55 @@ def main():
 	print(f.get())
 	f.set(9)
 	print(f.get())
+	
+
+	def fib_py(n):
+		if n <= 1:
+			return n
+		else:
+			return (fib_py(n-1) + fib_py(n-2))
+
+	@njit
+	def fib_numba(n):
+		if n <=1:
+			return n
+		else:		
+			return (fib_numba(n-1) + fib_numba(n-2))
+	
+	N=10
+	fib_py_list= []
+	fib_numba_list=[]
+	fib_cpp_list=[]
+
+	f=Person(N)
+	f.fib()
+	def caller():
+		for i in range [30:45]:
+			fib_py_list(i).append
+		return fib_py_list
+	print(caller())
+
+
+
+# _age= 10
+# start=pc()
+# print(fib_py(_age))
+# end=pc()
+# print("fib_py:", end-start)
+# start=pc()
+# print(fib_numba(_age))
+# end=pc()
+# print("numba: ", end-start)
+	
+
+	
+# n= Person(_age)
+
+
+
+
+if __name__ == '__main__':
+	main()
 
 if __name__ == '__main__':
 	main()
--- UPPMA4/person.py
+++ UPPMA4/person.py
@@ -1,7 +1,8 @@
 """ Python interface to the C++ Person class """
-from numba import njit
+
 import ctypes
-from time import perf_counter as pc
+
+
 
 lib = ctypes.cdll.LoadLibrary('./libperson.so')
 
@@ -24,52 +25,3 @@ class Person(object):
 	def __del__(self):
 		return lib.Person_delete(self.obj)
 
-
-def main():
-	def fib_py(n):
-		if n <= 1:
-			return n
-		else:
-			return (fib_py(n-1) + fib_py(n-2))
-
-	@njit
-	def fib_numba(n):
-		if n <=1:
-			return n
-		else:		
-			return (fib_numba(n-1) + fib_numba(n-2))
-	
-	N=10
-	fib_py_list= []
-	fib_numba_list=[]
-	fib_cpp_list=[]
-
-	f=Person(N)
-	f.fib()
-	def caller():
-		for i in range [30:45]:
-			fib_py_list(i).append
-		return fib_py_list
-	print(caller())
-
-
-
-# _age= 10
-# start=pc()
-# print(fib_py(_age))
-# end=pc()
-# print("fib_py:", end-start)
-# start=pc()
-# print(fib_numba(_age))
-# end=pc()
-# print("numba: ", end-start)
-	
-
-	
-# n= Person(_age)
-
-
-
-
-if __name__ == '__main__':
-	main()
