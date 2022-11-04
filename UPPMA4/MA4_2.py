ee705cc7202e3d3982214ec6fc41dd314413ba61
Kevin Salazar Penarrieta <kesa3291@arrhenius.it.uu.se>
2022-11-04 18:50:47 +0100

    last TEst

--- UPPMA4/MA4_2.py
+++ UPPMA4/MA4_2.py
@@ -2,7 +2,7 @@
 from numba import njit
 from time import perf_counter as pc
 from person import Person
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 
 def main():
 	
@@ -28,37 +28,59 @@ def main():
 
 	f=Person(N)
 	f.fib()
-	def caller():
-		for i in range [30:45]:
-			fib_py_list(i).append
-		return fib_py_list
-	print(caller())
-	r= range(30, 45)
+	
+	r=range(10, 15)
+	l=range(20,30)
+	
+#Question 5: py and numba for n=20-30
+	for n in l:
+		print('N: ', n)
+		start = pc()
+		fib_py(n)
+		end = pc()
+		fib_py_list.append(end-start)
+		#print('fib_py: ', end-start)
+
+		start = pc()
+		fib_numba(n)
+		end = pc()
+		fib_numba_list.append(end - start)
+		#print('fib_numba: ', end-start)
+#Question 4: all for n=30-45
 	for n in r:
 		print('N: ', n)
 		start = pc()
 		fib_py(n)
 		end = pc()
 		fib_py_list.append(end-start)
-		print('fib_py: ', end-start)
+		#print('fib_py: ', end-start)
 
 		start = pc()
 		fib_numba(n)
 		end = pc()
 		fib_numba_list.append(end - start)
-		print('fib_numba: ', end-start)
+		#print('fib_numba: ', end-start)
 
 		start = pc()
 		f = Person(n)
 		f.fib()
 		end = pc()
 		fib_cpp_list.append(end - start)
-		print('fibc: ', end-start)
+		#print('fibc: ', end-start)
+	#Q6=[]
+	#ctest=47
+	#start=pc()
+	#f=Person(ctest)
+	#f.fib()
+	#end=pc()
+	#Q6.append(end-start)
+	
 
-	plot.plot(r, fib_py_list, label='Python')
-	plot.plot(r, fib_numba_list, label='Numba')
-	plot.plot(r, fib_cpp_list, label='C++')
-	plot.savefig('plot_MA42.png')
+	plt.plot(r, fib_py_list, label='Python')
+	plt.plot(r, fib_numba_list, label='Numba')
+	plt.plot(r, fib_cpp_list, label='C++')
+	plt.show()
+	plt.savefig('plot_MA42.png')
 
 #xaxs
 
