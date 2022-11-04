4c50b226852e33e88b7b0463747127f9f8b35e6d
KevProgram <salazar-kevin@hotmail.com>
2022-11-04 16:24:38 +0100

    cpp

--- UPPMA4/person.cpp
+++ UPPMA4/person.cpp
@@ -1,47 +1,103 @@
 #include <cstdlib>
 // Person class 
 
+// class Person{
+// 	public:
+// 		Person(int);
+// 		int get();
+// 		void set(int);
+// 		int fib();
+// 	private:
+// 		int age;
+// 		int n;
+// 		int fib_private();
+// 	};
+ 
+// Person::Person(int n){
+// 	age = n;
+// 	}
+ 
+// int Person::get(){
+// 	return age;
+// 	}
+ 
+// void Person::set(int n){
+// 	age = n;
+// 	}
+
+// int Person::fib(){
+// 	return fib_private();
+// 	}
+// int fib_private(int age){
+// 	if (age <=1){
+// 	return age;
+// 	}
+// 	else {
+// 	return  fib_private(age-1) + fib_private(age-2);
+// 	}
+// 	}
+
+
+
+// extern "C"{
+// 	Person* Person_new(int n) {return new Person(n);}
+// 	int Person_get(Person* person) {return person->get();}
+// 	void Person_set(Person* person, int n) {person->set(n);}
+// 	void Person_delete(Person* person){
+// 		if (person){
+// 			delete person;
+// 			person = nullptr;
+// 			}
+// 		}
+// 	}
+
+
+
+
+
+
+
 class Person{
 	public:
 		Person(int);
 		int get();
-		void set(int);
 		int fib();
+		void set(int);
 	private:
-		int age;
 		int n;
-		int fib_private();
+		int fibn(int);
 	};
  
-Person::Person(int n){
-	age = n;
+Person::Person(int in){
+	n = in;
 	}
  
 int Person::get(){
-	return age;
+	return n;
 	}
  
-void Person::set(int n){
-	age = n;
+void Person::set(int in){
+	n = in;
 	}
 
 int Person::fib(){
-	return fib_private();
+	return fibn(n);
+}
+
+int Person::fibn(int in){
+	if (in <= 1){
+		return (in);
+	} else {
+		return (fibn(in-1) + fibn(in-2));
 	}
-int fib_private(int age){
-	if (age <=1){
-	return age;
-	}
-	else {
-	return  fib_private(age-1) + fib_private(age-2);
-	}
-	}
+}
 
 
 
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
+	int Person_fib(Person* person) {return person->fib();}
 	void Person_set(Person* person, int n) {person->set(n);}
 	void Person_delete(Person* person){
 		if (person){
